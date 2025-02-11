cacc227f9daf8727056e77bd6b6e627376bcfb22
Bilal El Bari <bilal.bari@dana.id>
2025-02-11 23:20:18 +0100

    initial commit

---
+++ src/test/java/com/example/cashcard/CashCardJsonTests.java
@@ -0,0 +1,14 @@
+package com.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CashCardJsonTests {
+
+    @Test
+    void myFirstTest() {
+//        assertThat(1).isEqualTo(42);
+        assertThat(42).isEqualTo(42);
+    }
+}
