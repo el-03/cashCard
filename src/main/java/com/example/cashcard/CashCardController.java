e5a6beca611344fa38f4d15f6599efac0a4d088e
Bilal El Bari <bilal.bari@dana.id>
2025-02-17 09:12:38 +0100

    Adding a repository and spring data

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,17 +1,25 @@
 package com.example.cashcard;
 
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{requestId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
-        if (requestId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestId);
+
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         }
         return ResponseEntity.notFound().build();
     }
